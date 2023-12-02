16f442a449b4868557d7919fb16d28e452a12e49
Hubert <57830712+2bombel2@users.noreply.github.com>
2023-12-02 15:20:42 +0100

    final

--- app/Http/Controllers/CordsController.php
+++ app/Http/Controllers/CordsController.php
@@ -7,6 +7,7 @@ use App\Models\pin;
 use App\Models\pin_photos;
 use App\Models\photos;
 use App\Models\users;
+use Illuminate\Support\Facades\Auth;
 
 class CordsController extends Controller
 {
@@ -16,6 +17,7 @@ class CordsController extends Controller
 
 
     public function cords_display(){
+        Auth::loginUsingId(1);
         $place = pin::all(['id', 'name', 'longitude', 'latitude']);
 
         return response()->json($place);
@@ -23,9 +25,10 @@ class CordsController extends Controller
     
     public function cords_display_with_photos(Request $request)
     {
+        Auth::loginUsingId(1);
         $id = $request->input('id');
         // Retrieve longitude, latitude, and photo_id for the given id from the pin table
-        $pin = pin::select(['longitude', 'latitude', 'photo_id'])->find($id);
+        $pin = pin::select(['longitude', 'latitude', 'photo_id', 'name'])->find($id);
         
         if (!$pin) {
             // Handle the case where the pin with the specified id is not found
@@ -51,6 +54,7 @@ class CordsController extends Controller
 
     public function photos_display(Request $request)
     {
+        Auth::loginUsingId(1);
         
         $id = $request->input('id');    
         // Retrieve photos related to the pin using the photo_id
@@ -61,29 +65,44 @@ class CordsController extends Controller
     }
 
     public function user_info(){
+        Auth::loginUsingId(1);
+        $user = auth()->user();
+
+        return response()->json($user, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+    }
+
+    public function user_info_get(){
+        Auth::loginUsingId(1);
         $user = auth()->user();
 
         return response()->json($user, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
     }
 
     public function feed(){
+        Auth::loginUsingId(1);
         $user = auth()->user();
         $friendIds = $user->friends;
         $photos = photos::whereIn('user_id', explode(',', $friendIds))->take(30)->get();
-        return response()->json($photos, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+        return response()->json([
+            'photos' => $photos,
+            'loggged_user' => $user,
+        ], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
         
 
     }
 
     public function post_image(Request $request){
+        Auth::loginUsingId(1);
         $user = auth()->user();
         $pinid = $request->input('pinid');
         $name = $request->input('name');
         $description = $request->input('description');
         $base64 = $request->input('base64');
+        $string = '["'.$user->id.'"]';
         $photo = new photos;
         $photo->pin_id = $pinid;
         $photo->name = $name;
+        $photo->likes = $string;
         $photo->base64 = $base64;
         $photo->description = $description;
         $photo->user_id = $user->id;
@@ -92,17 +111,32 @@ class CordsController extends Controller
     }   
 
     public function like(Request $request){
+        Auth::loginUsingId(1);
         $user = auth()->user();
         $photoid = $request->input('photoid');
         $photo = photos::where('id', $photoid)->first();
         $likes = $photo->likes;
-        $newlikes = $likes . ", " . $user->id;
-        $photo->likes = $newlikes;
-        $photo->save();
-        return response()->json("true", JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
-    }   
+        $array = json_decode($likes, true);
+        $index = array_search($user->id, $array);
+        if ($index !== false) {
+                unset($array[$index]);
+                $array = array_values($array);
+                $photo->likes = $array;
+                $photo->save();
+            return response()->json("false", JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+        }
+            else{
+                
+            $array[] = ''.$user->id.'';
+            $array = array_values($array);
+            $photo->likes = $array;
+            $photo->save();
+            return response()->json("true", JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);    
+        }
+        }   
 
     public function pinfeed(Request $request){
+        Auth::loginUsingId(1);
         $pinid = $request->input('pinid');
         $photos = photos::where('pin_id', $pinid)->get();
 
@@ -110,11 +144,13 @@ class CordsController extends Controller
     }   
 
     public function hello(){
+        Auth::loginUsingId(1);
         $photos = photos::take(10)->orderBy('created_at', 'desc')->get();
         return response()->json($photos, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
     }
 
     public function friends(){
+        Auth::loginUsingId(1);
         $user = users::where('id', auth()->user()->id)->first();
         $friendIds = $user->friends;
         $friends = users::whereIn('id', explode(',', $friendIds))->get(['id', 'name', 'base64']);
@@ -122,6 +158,7 @@ class CordsController extends Controller
     }
 
     public function friendshow(Request $request){
+        Auth::loginUsingId(1);
         $id = $request->input('id');
         $user = users::where('id', $id)->first();
         $photos = photos::where('user_id', $user->id)->get(['id','pin_id','name', 'base64', 'likes', 'description']);
@@ -129,19 +166,43 @@ class CordsController extends Controller
     }
 
     public function friendfind(Request $request){
+
+        Auth::loginUsingId(1);
         $user = auth()->user();
         $friendname = $request->input('name');
-        $friendid = users::where('name', $friendname)->first()->id;
+        $friendid = users::where('name', $friendname)->first();
+        
         if(users::where('name', $friendname)->exists()){
+            $idid = $friendid->id;
             $user = users::where('id', $user->id)->first();
             $friends = $user->friends;
-            $newfriends = $friends . ", " . $friendid;
+            $newfriends = $friends . ", " . $idid;
+            $user->friends = $newfriends;
+            $user->save();
             return true;
+
         }
         else{
             return false;
         }
         
+    }
+
+    public function frienddelete(Request $request, string $friendname){
+        Auth::loginUsingId(1);
+        $user = auth()->user();
+        $friend = users::where('name', $friendname)->firstOrFail();
+        $id = $friend->id;
+        $array = explode(',', $user->friends);
+        $key = array_search($id, $array);
+
+        if ($key != false) {
+            unset($array[$key]);
+            $user->friends = implode(', ', $array);
+            $user->save();
+        }
+
+        return response()->json();
     }
 
 
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\CordsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,3 +18,22 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth')->group(function () {
+
+});
+
+Route::Delete('/friendfind/{friendname}', [CordsController::class, 'frienddelete']);
+Route::get('/hello', [CordsController::class, 'hello']);
+Route::post('/koordynaty', [CordsController::class, 'cords_display']);
+Route::post('/koordynat/{id}', [CordsController::class, 'cords_display_with_photos']);
+Route::post('/photos/{id}', [CordsController::class, 'photos_display']);
+Route::get('/userinfo', [CordsController::class, 'user_info_get']);
+Route::post('/userinfo', [CordsController::class, 'user_info']);
+Route::post('/feed', [CordsController::class, 'feed']);
+Route::post('/post_image', [CordsController::class, 'post_image']);
+Route::post('/like', [CordsController::class, 'like']);
+Route::post('/pinfeed/{id}', [CordsController::class, 'pinfeed']);
+Route::post('/friends', [CordsController::class, 'friends']);
+Route::post('/friends/{id}', [CordsController::class, 'friendshow']);
+Route::post('/friendfind', [CordsController::class, 'friendfind']);
